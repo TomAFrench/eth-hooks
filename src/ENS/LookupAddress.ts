5dc12c80460179d36074d2b662265f496cb3c362
Tom French <tom@tomfren.ch>
2020-07-21 14:44:31 +0100

    refactor: remove unneeded await in lookupAddress

--- src/ENS/LookupAddress.ts
+++ src/ENS/LookupAddress.ts
@@ -6,7 +6,7 @@ const lookupAddress = async (provider: Provider, address: string): Promise<strin
   try {
     // Accuracy of reverse resolution is not enforced.
     // We then manually ensure that the reported ens name resolves to address
-    const reportedName = await provider.lookupAddress(address);
+    const reportedName = provider.lookupAddress(address);
     const resolvedAddress = await provider.resolveName(reportedName);
     if (getAddress(address) === getAddress(resolvedAddress)) {
       return reportedName;
